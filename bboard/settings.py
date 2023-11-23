0cd5c9e2e25aaf9d4b5a728d44dbd7593b10efa1
RainDeath1 <david53947@mail.ru>
2023-11-24 00:49:04 +0600

    final_60th_home

--- bboard/settings.py
+++ bboard/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hx3m5ddle8klip(687vh*q3w5p-)(o4jp-jm(6ig*s)e78wec1'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
